6a2817b69a94edf1ecea4fbe25682dcca6505c21
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-02-22 22:19:12 +0800

    multidimensional array

--- numpy/first.py
+++ numpy/first.py
@@ -4,3 +4,12 @@ ti_25 = np.array(["sora", "nayla", "rita", "fitri"])
 
 for names in ti_25:
     print(names)
+
+print("\nMULTIDIMENSIONAL ARRAY")
+
+matrix = np.array([[[1, 2, 3], [4, 5 ,6], [7, 8, 9]],
+                  [[10, 11, 12], [13, 14, 15], [16, 17, 18]], 
+                  [[19, 20, 21], [22, 23, 24], [25, 26, 27]]])
+
+print(matrix.ndim)
+print(matrix.shape)
