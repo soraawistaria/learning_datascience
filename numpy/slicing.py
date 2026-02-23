8ab31be30f7368fae2efe7753bc8ec0271cac26b
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-02-23 21:33:12 +0800

    slicing

---
+++ numpy/slicing.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+fruit = np.array([["banana", "melon", "berries"],
+                  ["devil", "grape", "pineapple"]])
+
+print(fruit[:, 0]) #every column 1  #:, start  #0:3 = index 0 till 3
+print(fruit[0 : 2]) #start : end : step
+
