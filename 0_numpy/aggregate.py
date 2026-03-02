a17c937e28d30ad664b8f52050aeb9a415bd2e9b
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-03-02 22:16:37 +0800

    aggregate

--- 0_numpy/aggregate.py
+++ 0_numpy/aggregate.py
@@ -3,7 +3,7 @@ import numpy as np
 array1 = np.array([[1, 2, 3, 4, 5], 
                    [6, 7, 8, 9, 10]])
 
-print(np.sum(array1))
+print("sum =", np.sum(array1))
 print(np.mean(array1))
 print(np.std(array1))
 print(np.var(array1))
@@ -11,3 +11,5 @@ print(np.min(array1))
 print(np.max(array1))
 print(np.argmin(array1))
 print(np.argmax(array1))
+
+print(np.sum(array1, axis = 0)) #axis 0 = jumlahkan kolom, axis 1 = jumlahkan baris
