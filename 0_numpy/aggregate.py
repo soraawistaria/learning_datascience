5b80a8448b7539a79c9ca4c8d671722884eaf757
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-03-02 22:12:46 +0800

    agregate

---
+++ 0_numpy/aggregate.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+array1 = np.array([[1, 2, 3, 4, 5], 
+                   [6, 7, 8, 9, 10]])
+
+print(np.sum(array1))
+print(np.mean(array1))
+print(np.std(array1))
+print(np.var(array1))
+print(np.min(array1))
+print(np.max(array1))
+print(np.argmin(array1))
+print(np.argmax(array1))
