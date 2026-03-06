f16e4f1bebb7a12274103426974c0a8d65ec20d9
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-03-06 21:38:06 +0800

    filtering

--- 0_numpy/filtering.py
+++ 0_numpy/filtering.py
@@ -1,7 +1,14 @@
 import numpy as np
 
-ages = np.array([[15, 16, 17, 20, 24], [21, 19, 18, 18, 19]])
+ages = np.array([[15, 16, 17, 20, 24], 
+                 [21, 19, 18, 18, 19]])
 
 teenagers = ages[ages < 18]
 
+print(teenagers)
+
+#mempertahankan array shape
+print()
+teenagers = np.where(ages <= 18, ages, np.nan)
+                    # boolean statement, our variables, replace with 'what'
 print(teenagers)
---
+++ 0_numpy/random.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+rng = np.random.default_rng()
+
+print(rng.intergers(1, 7))
