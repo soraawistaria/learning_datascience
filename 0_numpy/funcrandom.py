f4b9082e0b0dfe2827e3818554f4fdaab874d7ae
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-03-06 22:25:25 +0800

    random np

--- 0_numpy/funcrandom.py
+++ 0_numpy/funcrandom.py
@@ -1,5 +1,24 @@
 import numpy as np
 
-rng = np.random.default_rng()
+rng = np.random.default_rng(seed=1) #1 kali random saja
 
-print(rng.integers(1, 7, size=3))
+print(rng.integers(1, 7, size=(3,2)))
+                  #start, end, row x column
+
+print()
+#random float number
+np.random.seed(seed=2)
+print(np.random.uniform(size=3))
+
+print()
+#shuffle an array
+rendem = np.random.default_rng()
+array = np.array([1, 2, 3, 4, 5])
+rendem.shuffle(array)
+print(array)
+
+#random pick
+print()
+sahur = np.array(["mie", "telur", "nasi", "energen", "kurma", "kopi"])
+choice = rng.choice(sahur)
+print(choice)
