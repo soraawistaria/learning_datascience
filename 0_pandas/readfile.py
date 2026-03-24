27016db5264b06789db7fce70fb645efbd9c77c1
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-03-24 21:37:35 +0800

    read csv and json file

---
+++ 0_pandas/readfile.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv("0_pandas/biodata.csv")
+
+
+
+df = df.drop('No Whatsapp', axis=1)
+df = df.drop('NIM', axis=1)
+df = df.drop('Timestamp', axis=1)
+df = df.drop('Agama', axis=1)
+
+for call in df['Nama Panggilan']:
+    call.capitalize()
+
+
+print(df.to_string())
