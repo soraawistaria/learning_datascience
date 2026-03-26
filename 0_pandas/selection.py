c4e05240a3be488a83cd7bab5284ef42b4b88974
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-03-26 20:23:13 +0800

    Update selection.py

--- 0_pandas/selection.py
+++ 0_pandas/selection.py
@@ -9,4 +9,7 @@ df = pd.read_csv('spotify_wrapped_2025_top50_songs.csv')
 
 dff = pd.read_csv('spotify_wrapped_2025_top50_songs.csv', index_col='artist')  #sehingga indexnya bukan 0-habis, tapi nama artist jadi indexnya. mempermudah pencarian
 
-print(dff.loc["Sabrina Carpenter"])
+print(dff.loc["Sabrina Carpenter", ["wrapped_2025_rank", "song_title"]])
+print("\n\n")
+
+print(dff.iloc[0:5])
