ce9c588567d25af5ee8750c48b7334245be5dff5
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-08-25 22:17:38 +0800

    agregasi pandas

---
+++ 0_pandas/aggregate.py
@@ -0,0 +1,18 @@
+# set values into a single summary value
+# used to summarize and analyze data
+
+import pandas as pd
+
+df = pd.read_csv('0_pandas/data_siswa_bb_tb.csv')
+
+print("\n============= MEAN =============")
+print(df.mean(numeric_only=True))
+
+print("\n============= SUM =============")
+print(df.sum(numeric_only=True))
+
+print("\n============= MIN =============")
+print(df.min(numeric_only=True))
+
+print("\n============= MAX =============")
+print(df.max(numeric_only=True))
